cc73bda08cc6931452666bd09c6e0aad139aef93
g-bolmida <gbolmida@georgebolmida.com>
2026-06-14 19:49:20 -0400

    ha to gitops

--- stacks.tf
+++ stacks.tf
@@ -20,11 +20,15 @@ resource "portainer_stack" "flaresolverr" {
 }
 
 resource "portainer_stack" "homeassistant" {
-  name            = "homeassistant"
-  deployment_type = "standalone"
-  method          = "file"
-  endpoint_id     = data.portainer_environment.local.id
-  stack_file_path = "./homeassistant/docker-compose.yml"
+  name                      = "homeaissistant"
+  deployment_type           = "standalone"
+  method                    = "repository"
+  endpoint_id               = data.portainer_environment.local.id
+  repository_url            = "https://github.com/g-bolmida/homelab-config"
+  repository_reference_name = "refs/heads/main"
+  file_path_in_repository   = "./homeassistant/docker-compose.yml"
+  git_repository_authentication = true
+  repository_git_credential_id = 6
 
   env {
     name  = "CONFIG_DIR"
@@ -72,24 +76,24 @@ resource "portainer_stack" "big_box_backrest" {
 
 variable "plex_claim" {}
 
-#resource "portainer_stack" "plex" {
-#  name                      = "plex"
-#  deployment_type           = "standalone"
-#  method                    = "repository"
-#  endpoint_id               = data.portainer_environment.big_box.id
-#  repository_url            = "https://github.com/g-bolmida/homelab-config"
-#  repository_reference_name = "refs/heads/main"
-#  file_path_in_repository   = "./plex/docker-compose.yml"
-#  git_repository_authentication = true
-#  repository_git_credential_id = 6
-#
-#  env {
-#    name  = "CONFIG_DIR"
-#    value = "/home/gbolmida/plex/config"
-#  }
-#
-#  env {
-#    name  = "PLEX_CLAIM"
-#    value = var.plex_claim
-#  }
-#}
+resource "portainer_stack" "plex" {
+  name                      = "plex"
+  deployment_type           = "standalone"
+  method                    = "repository"
+  endpoint_id               = data.portainer_environment.big_box.id
+  repository_url            = "https://github.com/g-bolmida/homelab-config"
+  repository_reference_name = "refs/heads/main"
+  file_path_in_repository   = "./plex/docker-compose.yml"
+  git_repository_authentication = true
+  repository_git_credential_id = 6
+
+  env {
+    name  = "CONFIG_DIR"
+    value = "/home/gbolmida/plex/config"
+  }
+
+  env {
+    name  = "PLEX_CLAIM"
+    value = var.plex_claim
+  }
+}
